7fe0dcd69def48a2e6ad4f4b692342aae2ce269a
JISOONLEE <46081043+JISOONLEE@users.noreply.github.com>
2021-04-09 14:24:27 +0900

    Add files via upload

---
+++ Dynamic Programming/[Q32]정수 삼각형.py
@@ -0,0 +1,12 @@
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(1, n):
+    for j in range(len(graph[i])):
+        if j == 0:
+            graph[i][j] = graph[i][j] + graph[i-1][j]
+        elif j == len(graph[i])-1:
+            graph[i][j] = graph[i][j] + graph[i-1][j-1]
+        else:
+            graph[i][j] = graph[i][j] + max(graph[i-1][j-1], graph[i-1][j])
+print(max(graph[n-1]))
