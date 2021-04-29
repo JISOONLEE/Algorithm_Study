73d7c6507a66b9bbcc8c4eae26d4a82339b7e108
JISOONLEE <46081043+JISOONLEE@users.noreply.github.com>
2021-04-29 15:31:20 +0900

    Create [3차] 압축.py

---
+++ Programmers/[3차] 압축.py
@@ -0,0 +1,20 @@
+def solution(msg):
+    answer = []
+    dic = {}
+
+    for i in range(26):
+        dic[chr(ord('A')+i)] = i+1
+
+    w, c = 0, 0
+    while True:
+        c += 1
+        if c == len(msg):
+            answer.append(dic[msg[w:c]])
+            break
+            
+        if msg[w:c+1] not in dic:
+            dic[msg[w:c+1]] = len(dic) + 1
+            answer.append(dic[msg[w:c]])
+            w = c
+
+    return answer
