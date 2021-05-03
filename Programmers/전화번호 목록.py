461fb2ceda2733987592b29f66d375e765fd9743
JISOONLEE <46081043+JISOONLEE@users.noreply.github.com>
2021-05-03 18:54:00 +0900

    Update 전화번호 목록.py

--- Programmers/전화번호 목록.py
+++ Programmers/전화번호 목록.py
@@ -3,4 +3,8 @@ def solution(phone_book):
     for i, j in zip(phone_book, phone_book[1:]):
         if i == j[:len(i)]:
                 return False
+        """
+        if p2.startswith(p1):
+            return False
+        """
     return True
