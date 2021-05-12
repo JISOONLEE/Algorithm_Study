ffb65a6bac458e85e8ef3fc8aeb6393109013580
JISOONLEE <46081043+JISOONLEE@users.noreply.github.com>
2021-05-12 18:03:51 +0900

    Add files via upload

---
+++ 이것이 코딩테스트다/[Q5]볼링공 고르기.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+k = list(map(int, input().split()))
+answer = 0
+for i in range(len(k)):
+    first = k[i]
+    temp = k[i+1:]
+    answer += (len(temp)-temp.count(first))
+
+print(answer)
