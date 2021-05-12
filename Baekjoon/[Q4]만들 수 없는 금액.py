4beaef0f834f1c0bc1086e9e13182c9508ba2f5b
JISOONLEE <46081043+JISOONLEE@users.noreply.github.com>
2021-05-12 16:46:22 +0900

    Add files via upload

---
+++ Baekjoon/[Q4]만들 수 없는 금액.py
@@ -0,0 +1,30 @@
+from itertools import permutations
+
+n = int(input())
+m = list(map(int, input().split()))
+
+"""
+result = []
+for i in range(n+1):
+    for j in permutations(m, i):
+        result.append(sum(j))
+result.sort()
+result = list(set(result))
+
+answer = 0
+for i in range(1, len(result)):
+    if result[i-1]+1 != result[i]:
+        answer = result[i-1] + 1
+        break
+print(answer)
+"""
+
+m.sort()
+
+target = 1
+for x in m:
+    if target < x :
+        break
+    target += x
+
+print(target)
