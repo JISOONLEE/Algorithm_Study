94aaf4124e47ae60dd9eb06548c635f10c4df803
JISOONLEE <46081043+JISOONLEE@users.noreply.github.com>
2021-05-29 19:59:41 +0900

    Update 로또의 최고 순위와 최저 순위.py

--- Programmers/로또의 최고 순위와 최저 순위.py
+++ Programmers/로또의 최고 순위와 최저 순위.py
@@ -1,3 +1,4 @@
+"""
 def solution(lottos, win_nums):
     ans_min = []
     for win_num in win_nums:
@@ -10,3 +11,9 @@ def solution(lottos, win_nums):
         ans_max = 1
     answer = [7-ans_max, 7-len(ans_min)]
     return answer
+"""
+def solution(lottos, win_nums):
+    rank = [1,2,3,4,5,6,6]
+    min_num = len(set(win_nums) - set(lottos))
+    max_num = min_num - lottos.count(0)
+    return rank[max_num], rank[min_num]
